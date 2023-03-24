4a9066cfffe3b913f6fb23168fe7f9a520565b07
sourabhpatole <sourabhpatole@outlook.com>
2023-03-24 14:41:07 +0530

    #

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+require("dotenv").config();
 const myConsole = new console.Console(fs.createWriteStream("./logs.txt"));
 const VerifyToken = (req, res) => {
   try {
-    let accessToken =
-      "EAAIvPYSLqTIBAOAq7IoKaq1mN20nsYZC5mRBc0DeyGGI3bdr9o6OPncZAdYVUZB5ZCK8IG4JZCksvZCGNoz7XJmJZAz3aEBYYUmAbIqnZBOFLNUXwS8TfoEBlZACNXfpcfETEXw1Gja4jdGbBFcsMyhkOJ3SOE08gXQnMpYeSSdDz3RImCmnToZCnC1lNl0f8DD6eZCDzIDBYKG2WcX4bYxucUn2IsE6uvWrxsZD";
+    let accessToken = process.env.TOKEN;
+
     let token = req.query["hub.verify_token"];
     let challenge = req.query["hub.challenge"];
     if (challenge != null && token != null && token == accessToken) {
